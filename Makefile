08d3c9f7883693c3bde530f9bda7bb9fa3c3bbf8
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2024-09-25 22:10:45 +0400

    prepare environment

---
+++ Makefile
@@ -0,0 +1,9 @@
+install:
+	npm ci
+publish:
+	npm publish
+link:
+	npm link
+lint:
+	npx eslint .
+	
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,12 +3,9 @@ const { program } = require('commander');
 program
   .option('-V, --version', 'output the version number')
   .option('-h, --help', 'output usage information')
-  .description('Compares two configuration files and shows a difference.')
- 
+  .description('Compares two configuration files and shows a difference.');
 
 program.parse();
 
-
 const options = program.opts();
 console.log(options);
-
