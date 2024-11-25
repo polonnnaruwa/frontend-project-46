73b07b73fb576fb0a0094d11551405b91af0fd8d
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2024-11-25 15:50:37 +0400

    extname

--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,10 @@
 import { readFileSync } from 'node:fs';
+import { extname } from 'node:path';
 import yaml from 'js-yaml';
 
 export default (filePath) => {
   const fileContent = readFileSync(filePath, 'utf8');
-  if (filePath.endsWith(".yml"))  {
+  if (extname(filePath) === '.yml' || extname(filePath) === '.yaml')  {
     return yaml.load(fileContent);
   } else {
     return JSON.parse(fileContent);
