49b00fc3dfdc0053be01e4c678947a0582d0b536
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2024-11-26 14:01:50 +0400

    nested tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,22 +9,12 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff 1-2', () => {
+test('genDiff json', () => {
     const file1path = getFixturePath('file1.json');
     const file2path = getFixturePath('file2.json');
     const expectedResult = readFile('result');
 
     const actualResult = genDiff(file1path, file2path);
-    expect(actualResult).toBe(expectedResult);
-});
-
-
-test('genDiff 1-3', () => {
-    const file1path = getFixturePath('file1.json');
-    const file3path = getFixturePath('file3.json');
-    const expectedResult = readFile('result1-3');
-
-    const actualResult = genDiff(file1path, file3path);
     expect(actualResult).toBe(expectedResult);
 });
 
