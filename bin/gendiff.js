fe1dcb92ff89c3cb9b820ae6a99ba27e3b76e2e6
Anisia Shvetcova <polonnnaruwa@hotmail.com>
2024-12-02 16:13:39 +0400

    diff nested

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,11 +5,12 @@ import genDiff from '../src/gendiff.js';
 program
   .option('-V, --version', 'output the version number')
   .option('-h, --help', 'output usage information')
+  .option('-f, --formatter <formatter>', 'output formatter', 'stylish')
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>', 'file path 1')
   .argument('<filepath2>', 'file path 2')
-  .action((filepath1, filepath2) => {
-    const text = genDiff(filepath1, filepath2);
+  .action((filepath1, filepath2, options) => {
+    const text = genDiff(filepath1, filepath2, options.formatter);
     console.log(text);
   });
 
--- src/gendiff.js
+++ src/gendiff.js
@@ -3,6 +3,11 @@ import parseFile from './parsers.js';
 function removeDublicates(array) {
   return [...new Set(array)];
 }
+
+function isObject(value) {
+  return typeof value === 'object' && !Array.isArray(value) && value !== null
+}
+
 const makeInnerStruct = (object1, object2) => {
   const objectKeys1 = Object.keys(object1);
   const objectKeys2 = Object.keys(object2);
@@ -14,6 +19,15 @@ const makeInnerStruct = (object1, object2) => {
   const innerStruct = [];
   objectKeys.sort().forEach((objectKey) => {
     if (Object.hasOwn(object1, objectKey) && !Object.hasOwn(object2, objectKey)) {
+      if (isObject(object1[objectKey])) {
+        const obj = {
+          action: 'removed', 
+          property: objectKey, 
+          old: makeInnerStruct(object1[objectKey], object1[objectKey]),
+        };
+        innerStruct.push(obj);
+        return;
+      }
       const obj = {
         action: 'removed', 
         property: objectKey, 
@@ -22,6 +36,15 @@ const makeInnerStruct = (object1, object2) => {
       innerStruct.push(obj);
     }
     if (!Object.hasOwn(object1, objectKey) && Object.hasOwn(object2, objectKey)) {
+      if (isObject(object2[objectKey])) {
+        const obj = {
+          action: 'added', 
+          property: objectKey, 
+          new: makeInnerStruct(object2[objectKey], object2[objectKey]),
+        };
+        innerStruct.push(obj);
+        return;
+      }
       const obj = {
         action: 'added',
         property: objectKey,
@@ -30,6 +53,17 @@ const makeInnerStruct = (object1, object2) => {
       innerStruct.push(obj);
     }
     if (Object.hasOwn(object1, objectKey) && Object.hasOwn(object2, objectKey)) {
+
+      if (isObject(object1[objectKey]) && isObject(object2[objectKey])) {
+        const obj = {
+          action: 'kept',
+          property: objectKey,
+          old: makeInnerStruct(object1[objectKey], object2[objectKey]),
+        };
+        innerStruct.push(obj);
+        return;
+      }
+
       if (object1[objectKey] === object2[objectKey]) {
         const obj = {
           action: 'kept',
@@ -41,8 +75,8 @@ const makeInnerStruct = (object1, object2) => {
         const obj = {
           action: 'changed',
           property: objectKey,
-          old: object1[objectKey],
-          new: object2[objectKey],
+          old: isObject(object1[objectKey]) ? makeInnerStruct(object1[objectKey], object1[objectKey]) : object1[objectKey],
+          new: isObject(object2[objectKey]) ? makeInnerStruct(object2[objectKey], object2[objectKey]) : object2[objectKey],
         };
         innerStruct.push(obj);
       }
@@ -51,29 +85,67 @@ const makeInnerStruct = (object1, object2) => {
   return innerStruct;
 }
 
-const formater = (innerStruct) => {
+const stylish = (innerStruct, property='', depth=0) => {
   const output = [];
-  output.push('{');
+  let prefix = '';
+  for (let i = 0; i < depth; i++) {
+    prefix += '    ';
+  }
+
+  if (property) {
+    const newPrefix = prefix.substring(4);
+    output.push(`${newPrefix}${property}: {`);
+  } else {
+    output.push(`${prefix}{`);
+  }
+
   innerStruct.forEach((obj) => {
     if(obj.action === 'added') {
-      output.push(`  + ${obj.property}: ${obj.new}`);
+      if (Array.isArray(obj.new)) {
+        output.push(stylish(obj.new, `  + ${obj.property}`, depth+1));
+      } else {
+        output.push(`${prefix}  + ${obj.property}: ${obj.new}`);
+      }
+
     } else if (obj.action === 'removed') {
-      output.push(`  - ${obj.property}: ${obj.old}`);
+      if (Array.isArray(obj.old)) {
+        output.push(stylish(obj.old, `  - ${obj.property}`, depth+1));
+      } else {
+        output.push(`${prefix}  - ${obj.property}: ${obj.old}`);
+      }
     } else if (obj.action === 'kept') {
-      output.push(`    ${obj.property}: ${obj.old}`);
+      if (Array.isArray(obj.old)) {
+        output.push(stylish(obj.old, `    ${obj.property}`, depth+1));
+      } else {
+        output.push(`${prefix}    ${obj.property}: ${obj.old}`);
+      }
     } else if (obj.action === 'changed') {
-      output.push(`  - ${obj.property}: ${obj.old}`);
-      output.push(`  + ${obj.property}: ${obj.new}`);
+      if (Array.isArray(obj.old)) {
+        output.push(stylish(obj.old, `  - ${obj.property}`, depth+1));
+      } else {
+        output.push(`${prefix}  - ${obj.property}: ${obj.old}`);
+      }
+      if (Array.isArray(obj.new)) {
+        output.push(stylish(obj.new, `  + ${obj.property}`, depth+1));
+      } else {
+        output.push(`${prefix}  + ${obj.property}: ${obj.new}`);
+      }
     }
   });
-  output.push('}');
+  output.push(`${prefix}}`);
   return output.join('\n');
 }
-export default function genDiff(filepath1, filepath2) {
+
+const formatters = {
+  stylish: stylish,
+};
+
+export default function genDiff(filepath1, filepath2, formatter='stylish') {
   const left = parseFile(filepath1);  
   const right = parseFile(filepath2);
   const innerStruct = makeInnerStruct(left, right);
-  const output = formater(innerStruct);
+  const formatterFunc = formatters[formatter];
+  const output = formatterFunc(innerStruct);
   return output;
 }
 
@@ -117,7 +189,7 @@ export default function genDiff(filepath1, filepath2) {
 // [
 //   {action: 'removed', property: 'setting2', old: 200},
 //   {action: 'added', property: 'follow', new: false},
-//   {action: 'kept', property: 'foo', old: 'foo', new: 'foo'},
+//   {action: 'kept', property: 'foo', old: 'foo'},
 //   {action: 'changed', property: 'baz', old: 'bas', new: 'bars'},
 //   {action: 'changed', property: 'zzz', old: {sss: 'sss'}, new: []},
 //   {
@@ -129,6 +201,7 @@ export default function genDiff(filepath1, filepath2) {
 //       {action: 'changed', property: 'baz', old: 'bas', new: 'bars'},
 //       {action: 'kept', property: 'foo', old: 'foo', new: 'foo'},
 //       {action: 'changed', property: 'nest', old: {key: 'value'}, new: 'str'},
+//       {action: 'kept', property: 'foo', children: []},
 //     ]
 //   },
 // ]
@@ -137,3 +210,4 @@ export default function genDiff(filepath1, filepath2) {
 // + follow : false
 //   foo : foo
 // - baz : old bas new bars
+ 
